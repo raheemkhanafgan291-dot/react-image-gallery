af129dbc3f27eff5033bc45df0a20f86ef76718d
Rahim <raheeemkhanafgan291@gmail.com>
2026-01-11 11:39:16 +0530

    Fix Netlify build warnings

--- src/props/FirstComp.jsx
+++ src/props/FirstComp.jsx
@@ -1,65 +1,70 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 const FirstComp = () => {
+  const [userData, setUserData] = useState([]);
+  const [index, setIndex] = useState(1);
+  const [loading, setLoading] = useState(true);
 
-  const[userData,setUSerData]=useState([]);
-  const[index, setIndex]=useState(1)
+  const getData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`);
+      setUserData(response.data);
+      setLoading(false);
+    } catch (err) {
+      console.error(err);
+      setLoading(false);
+    }
+  };
 
-  const   getData= async () => {
-    const resopnse= await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`)
-   setUSerData(resopnse.data)
-  }
-
-
-    useEffect(function(){
-      getData()
-    },[index])
-
-
-
-   let printUserData= <h3 className='Loading'>Loading...</h3>
-   
-
-   if(userData.length>0){
-    printUserData=userData.map(function(elem,idx){
-
-
-      return <div key={elem.idx}>
-        
-        <a href={elem.url} >
-        <div className='img-div'>
-        <img className='img-box' src={elem.download_url} alt=''/>
-      </div>
-      <h2>{elem.author}</h2>
-      </a>
-      </div>
-    })
-   }
-
+  useEffect(() => {
+    getData();
+  }, [index]);
 
   return (
     <div className='mainsection'>
       <div className='user-data'>
-         {printUserData}
+        {loading ? (
+          <h3 className='Loading'>Loading...</h3>
+        ) : (
+          userData.map((elem, idx) => (
+            <div key={elem.id} className='img-div-wrapper'>
+              <a href={elem.url} target="_blank" rel="noreferrer">
+                <div className='img-div'>
+                  <img className='img-box' src={elem.download_url} alt={elem.author} />
+                </div>
+                <h2>{elem.author}</h2>
+              </a>
+            </div>
+          ))
+        )}
       </div>
 
       <div className='btnsec'>
-      <button className='btn' onClick={()=>{
-           if(index>1){
-            setIndex(index-1)
-           }
-      }}>
-        Perious
+        <button
+          className='btn'
+          onClick={() => {
+            if (index > 1) setIndex(index - 1);
+          }}
+        >
+          Previous
         </button>
+
         <h1>{index}</h1>
-      <button className='btn1' onClick={()=>{
-        setIndex(index+1)
-      }}>Next </button>
+
+        <button
+          className='btn1'
+          onClick={() => {
+            setIndex(index + 1);
+          }}
+        >
+          Next
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FirstComp
+export default FirstComp;
